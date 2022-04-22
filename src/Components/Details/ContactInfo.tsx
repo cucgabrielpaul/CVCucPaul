fef0c6cb4f1dec76e698555dd833ad0aceef9956
cucgabrielpaul <cucgabrielpaul@yahoo.com>
2022-04-22 20:08:24 +0300

    Added SocialMedia details

--- src/Components/Details/ContactInfo.tsx
+++ src/Components/Details/ContactInfo.tsx
@@ -5,57 +5,25 @@ import {
   ListItemAvatar,
   ListItemText,
 } from "@mui/material";
-import { SiGooglemaps } from "@react-icons/all-files/si/SiGooglemaps";
-import { AiFillYahoo } from "@react-icons/all-files/ai/AiFillYahoo";
-import { FcCallback } from "@react-icons/all-files/fc/FcCallback";
-import { FaBabyCarriage } from "@react-icons/all-files/fa/FaBabyCarriage";
 
-const ContactInfo = () => {
+const ContactInfo = (props: any) => {
+  const { infos } = props;
+
   return (
     <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <SiGooglemaps />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText
-          primary="Str. Observatorului, Nr.13"
-          secondary="City Cluj-Napoca, County Cluj"
-        />
-      </ListItem>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <FaBabyCarriage />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText
-          primary="09/Sept/1992"
-          secondary="City Cluj-Napoca, County Cluj"
-        />
-      </ListItem>
-      <ListItem
-        button
-        key="Email"
-        component="a"
-        href="mailto:cucgabrielpaul@yahoo.com"
-      >
-        <ListItemAvatar>
-          <Avatar>
-            <AiFillYahoo />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="cucgabrielpaul@yahoo.com" />
-      </ListItem>
-      <ListItem button key="Phone" component="a" href="tel:+40755339912">
-        <ListItemAvatar>
-          <Avatar>
-            <FcCallback />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="0755339912" />
-      </ListItem>
+      {infos.map((info: any) => (
+        <ListItem
+          button={info.button}
+          key={info.key}
+          component={info.component}
+          href={info.href}
+        >
+          <ListItemAvatar>
+            <Avatar>{info.icon}</Avatar>
+          </ListItemAvatar>
+          <ListItemText primary={info.primary} secondary={info.secondary} />
+        </ListItem>
+      ))}
     </List>
   );
 };
--- src/Components/Details/DetailsCard.tsx
+++ src/Components/Details/DetailsCard.tsx
@@ -6,8 +6,15 @@ import Collapse from "@mui/material/Collapse";
 import IconButton, { IconButtonProps } from "@mui/material/IconButton";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import MyAvatar from "./MyAvatar";
-import SocialMedia from "./SocialMedia";
 import ContactInfo from "./ContactInfo";
+import { SiGooglemaps } from "@react-icons/all-files/si/SiGooglemaps";
+import { AiFillYahoo } from "@react-icons/all-files/ai/AiFillYahoo";
+import { FcCallback } from "@react-icons/all-files/fc/FcCallback";
+import { FaBabyCarriage } from "@react-icons/all-files/fa/FaBabyCarriage";
+import { SiFacebook } from "@react-icons/all-files/si/SiFacebook";
+import { SiInstagram } from "@react-icons/all-files/si/SiInstagram";
+import { SiLinkedin } from "@react-icons/all-files/si/SiLinkedin";
+import { SiGithub } from "@react-icons/all-files/si/SiGithub";
 
 interface ExpandMoreProps extends IconButtonProps {
   expand: boolean;
@@ -26,6 +33,84 @@ const ExpandMore = styled((props: ExpandMoreProps) => {
     duration: theme.transitions.duration.shortest,
   }),
 }));
+
+const infos = [
+  {
+    icon: <SiGooglemaps />,
+    primary: "Str. Observatorului, Nr.13",
+    secondary: "City Cluj-Napoca, County Cluj",
+    button: false,
+    key: "Address",
+    component: "",
+    href: "",
+  },
+  {
+    icon: <FaBabyCarriage />,
+    primary: "13/Sept/1992",
+    secondary: "City Cluj-Napoca, County Cluj",
+    button: false,
+    key: "BornDate",
+    component: "",
+    href: "",
+  },
+  {
+    icon: <AiFillYahoo />,
+    primary: "cucgabrielpaul@yahoo.com",
+    secondary: "",
+    button: true,
+    key: "Email",
+    component: "a",
+    href: "mailto:cucgabrielpaul@yahoo.com",
+  },
+  {
+    icon: <FcCallback />,
+    primary: "0755339912",
+    secondary: "",
+    button: true,
+    key: "PhoneNo",
+    component: "a",
+    href: "tel:+40755339912",
+  },
+];
+
+const socialMedia = [
+  {
+    icon: <SiFacebook />,
+    primary: "Facebook",
+    secondary: "www.facebook.com/cucgabrielpaul",
+    button: true,
+    key: "Facebook",
+    component: "a",
+    href: "https://www.facebook.com/cucgabrielpaul/",
+  },
+  {
+    icon: <SiInstagram />,
+    primary: "Instagram",
+    secondary: "www.instagram.com/cucgabrielpaul",
+    button: true,
+    key: "Instagram",
+    component: "a",
+    href: "https://www.instagram.com/cucgabrielpaul/",
+  },
+  {
+    icon: <SiLinkedin />,
+    primary: "LinkedIn",
+    secondary: "www.linkedin.com/in/cucgabrielpaul",
+    button: true,
+    key: "LinkedIn",
+    component: "a",
+    href: "https://www.linkedin.com/in/cucgabrielpaul/",
+  },
+  {
+    icon: <SiGithub />,
+    primary: "GitHub",
+    secondary: "https://github.com/cucgabrielpaul",
+    button: true,
+    key: "GitHub",
+    component: "a",
+    href: "https://github.com/cucgabrielpaul",
+  },
+];
 
 const DetailsCard = () => {
   const [expanded, setExpanded] = useState(false);
@@ -54,8 +139,8 @@ const DetailsCard = () => {
           }}
         >
           <MyAvatar />
-          <ContactInfo />
-          <SocialMedia />
+          <ContactInfo infos={infos} />
+          <ContactInfo infos={socialMedia} />
         </CardActions>
       </Collapse>
     </Card>
